b977b2d805fca5e7618c1455e9c81bdc0dbc3f9c
gilangadst <144768015+gilangadst@users.noreply.github.com>
2024-11-14 20:19:31 +0700

    Add files via upload

---
+++ Activity6_5041564/src/main/java/me/gilang/repository/MahasiswaRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package me.gilang.repository;
+
+
+import me.gilang.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Integer> {
+    // Additional query methods (if needed) can be added here
+}
